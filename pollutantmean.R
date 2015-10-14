1a5bdcb3a105dde693c8e2b9459d0a15c77af7ad
Boyd Ingalls <boyd.ingalls@gmail.com>
2015-10-14 14:38:33 -0500

    tested version with code commentary

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,9 +1,11 @@
-pollutantmean <- function(directory, pollutant, id = 1:332) {
-        list_files <- list.files(directory, full.names = TRUE)
-        pol <- data.frame()
+pollutantmean <- function(directory, pollutant, id = 1:332) { #creates function
+        list_files <- list.files(directory, full.names = TRUE) # creates a list of table names
+        pol <- data.frame() # creates a data frame
         for (i in id) {
-                pol <- rbind(pol, read.csv(list_files[i]))
+                pol <- rbind(pol, read.csv(list_files[i])) # combines the indicated data in the data frame "pol"
         }
-        pol_subset <- pol[which(pol[ , "ID"] %in% id), ]
-        mean(pol_subset[ , pollutant], na.rm = TRUE)
+        pol_subset <- pol[which(pol[ , "ID"] %in% id), ] #creates a subset of pol called "pol_subset that incudes the data tables indicated by the "id" input
+        mean(pol_subset[ , pollutant], na.rm = TRUE) #gives the mean of the column in "pol_subset" whose column header is indicated by the "pollutant" entered
 }
+
+
