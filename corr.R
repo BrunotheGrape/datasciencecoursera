d0f22df6b4b19594af5b99c4adcdbee8fdefc47e
Boyd Ingalls <boyd.ingalls@gmail.com>
2015-10-17 13:09:41 -0500

    final code with comments

--- corr.R
+++ corr.R
@@ -1,25 +1,23 @@
 
         
 
-corr <- function(directory, threshold, id = 1:332) {
-        list_files <- list.files(directory, full.names = TRUE)
-        pol <- data.frame()
+corr <- function(directory, threshold, id = 1:332) { #creates function
+        list_files <- list.files(directory, full.names = TRUE) #creates data frame to recieve variables
+        pol <- data.frame()# At this point I create all the data frames I will need going forward
         rslt <- data.frame()
         cr <- data.frame()
-        b <- data.frame()
-        for (i in id) {
+        for (i in id) { #This function tests each table for the threshold 
                 
                 pol <- read.csv((list_files[i]))
                 q <- sum(complete.cases(pol))
-                if (q > threshold) {
-                        
+                if (q > threshold) { #This function calculates the correlation fortables that pass the threshold test and adds the results to dataframe "cr"
                         cr <- rbind(cr,cor(pol[, 2], pol[3], use = "pairwise.complete.obs"))
                         
                 }
                 
         }
         
-        x <-cr[1:6, ]
+        x <-cr[1:6, ] #these lines manage the output
         print(x)
         names(cr) <- ""
         summary(cr)
