f48360f82ac17ae0f96202dee6ce39bc31cc73e6
Boyd Ingalls <boyd.ingalls@gmail.com>
2015-10-17 15:39:47 -0500

    editied the script to produce a vector and not a dataframe for the output

--- corr.R
+++ corr.R
@@ -5,24 +5,22 @@ corr <- function(directory, threshold = 0, id = 1:332) { #creates function
         list_files <- list.files(directory, full.names = TRUE) #creates data frame to recieve variables
         pol <- data.frame()# At this point I create all the data frames I will need going forward
         rslt <- data.frame()
-        cr <- data.frame()
+        cr <- c()
         for (i in id) { #This function tests each table for the threshold 
                 
                 pol <- read.csv((list_files[i]))
                 q <- sum(complete.cases(pol))
-                if (q > threshold) { #This function calculates the correlation fortables that pass the threshold test and adds the results to dataframe "cr"
-                        cr <- rbind(cr,cor(pol[, 2], pol[3], use = "pairwise.complete.obs"))
+                if (q > threshold) { #This function calculates the correlation for tables that pass the threshold test and adds the results to vector "cr"
+                        cr <- c(cr,cor(pol[, 2], pol[3], use = "pairwise.complete.obs"))
                         
                 }
                 
         }
-        
-        x <-cr[1:6, ] #these lines manage the output
-        print(x)
-        names(cr) <- ""
-        summary(cr)
+
+        y <- head(cr) #these lines manage output
+        print(y)
+        summary(cr, digits = 3)
        
 }
-
 
 
