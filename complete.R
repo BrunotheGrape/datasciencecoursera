98cd6b26524cd3a9d6a59d1d9f94474ddced46c5
Boyd Ingalls <boyd.ingalls@gmail.com>
2015-10-18 00:02:24 -0500

    Final version with updates to correctly format output. This version passed the test.

--- complete.R
+++ complete.R
@@ -2,14 +2,15 @@ complete <- function(directory, id = 1:332) { # creates function
         list_files <- list.files(directory, full.names = TRUE) # generates list
         com <- data.frame() # generates data frame
         comn <- data.frame(id = character(0), nob = character(0))
-        comn <- c("id", "nobs") #creates column headings for matrix
+        #comn <- c("id", "nobs") #creates column headings for matrix
         for (i in id) { # generic funtcion to read the data into the matrix
                 com <- read.csv(list_files[i]) # loads data into the data frame
                 com_subset <- com[which(com[ , "ID"] %in% id), ] #subsets the data frame per the entered criteria
                 x <- sum(complete.cases(com_subset)) # creates variable x to contain sum of selected columns
                 y <- c(i, x)  #creates y vector to select rows
+                print(y)
                 comn <- rbind(comn, y) # combines rows into the matrix comn
         }
-       
-        print(comn) # prints results
+       names(comn) <- c("id", "nobs")
+        return(comn) # prints results
 }
