a62b3e36b099df3783455288f2b4ea09d445c27c
Boyd Ingalls <boyd.ingalls@gmail.com>
2015-10-18 14:43:01 -0500

    looping function to convert numeric values

--- best.R
+++ best.R
@@ -1,15 +1,29 @@
-r <- read.csv("outcome-of-care-measures.csv", header = TRUE)
-r[,1:6]
-str(r)
-names(r)
-r[,7]
-r[c(2, 7, 11)]
-a <- r[2]
-b <- r[7]
-c <- r[11]
-d <- cbind(a, b)
-d <- cbind(d, c)
-d[1:6,]
-a
-b
-c
+for (i in 1:10) {
+r <- data.frame()
+rc <- data.frame()
+r <- read.table("rprog-data-ProgAssignment3-data/outcome-of-care-measures.csv", header = TRUE, sep = ",",  stringsAsFactors = FALSE)
+
+
+rc <- (((r[, c(1, 2, 7, 11, 17, 23)])))
+colnames(rc) [4] <- "heart attack"
+colnames(rc) [5] <- "heart failure"
+colnames(rc) [6] <- "pneumonia"
+
+
+        rslt <- c(rslt, as.numeric(as.character(rc[i, 4])))
+
+
+        
+      
+}
+
+
+best <- function(state, outcome = "heart attack") {
+        
+        
+}
+rc[1:6, 4]
+b <- rc[4]
+class(b)
+b <- as.numeric.factor(b)
+cr[1:5,]
